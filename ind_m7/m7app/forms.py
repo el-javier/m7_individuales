aed05176a14463b670fe1e861a897228220b4b83
Javier <jsotom@gmail.com>
2023-07-06 09:55:39 -0400

    actualizacion 3

--- ind_m7/m7app/forms.py
+++ ind_m7/m7app/forms.py
@@ -8,3 +8,14 @@ from django.contrib.auth.models import User, Group
 class LoginForm(forms.Form):
     username = forms.CharField(label='Nombre de usuario')
     password = forms.CharField(widget=forms.PasswordInput, label='Contraseña')
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=100)
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs['placeholder'] = 'Nombre de usuario'
+        self.fields['password'].widget.attrs['placeholder'] = 'Contraseña'
+
