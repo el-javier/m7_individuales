0959b71e9de354408946ecdf5d4bff9e4186e97e
Javier <jsotom@gmail.com>
2023-07-06 12:02:36 -0400

    actualizacion abp2

--- ind_m7/m7app/models.py
+++ ind_m7/m7app/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+# Definiendo la clase para las tablas de usuario , recogeran datos de nombre, email, edad y fecha de creacion.
+
+class Etiqueta(models.Model):
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
+class Tarea(models.Model):
+    ESTADO_CHOICES = (
+        ('pendiente', 'Pendiente'),
+        ('en_progreso', 'En Progreso'),
+        ('completada', 'Completada'),
+    )
+
+    titulo = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    fecha_vencimiento = models.DateField()
+    estado = models.CharField(max_length=12, choices=ESTADO_CHOICES)
+    etiqueta = models.ForeignKey(Etiqueta, on_delete=models.CASCADE)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.titulo
+
